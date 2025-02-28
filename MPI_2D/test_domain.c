4669ee3df9643884c9f6b344e11c0d9783608438
Zach Nusbickel <zn9800@g.rit.edu>
2025-02-28 12:02:32 -0500

    Adding exchange_ghostzones and MPI_Isendrecv to exchange_ghostzones

--- MPI_2D/test_domain.c
+++ MPI_2D/test_domain.c
@@ -7,6 +7,10 @@
 
 static void fill_gf_test_function(struct ngfs_2d *gfs);
 static void print_gf_test_function(struct ngfs_2d *gfs);
+static void exchange_ghost_cells(struct ngfs_2d *gfs);
+int MPI_Isendrecv(const void *sendbuf, int sendcount, MPI_Datatype sendtype, int dest, int sendtag, 
+                  void *recvbuf, int recvcount, MPI_Datatype recvtype, int source, int recvtag, MPI_Comm comm,
+                  MPI_Request *request);
 
 int main(int argc, char **argv)
 {
@@ -108,6 +112,17 @@ static void fill_gf_test_function(struct ngfs_2d *gfs)
     }
 }
 
+int MPI_Isendrecv(const void *sendbuf, int sendcount, MPI_Datatype sendtype, int dest, int sendtag, 
+		  void *recvbuf, int recvcount, MPI_Datatype recvtype, int source, int recvtag, MPI_Comm comm,
+                  MPI_Request *request) 
+{
+    MPI_Isend(sendbuf, sendcount, sendtype, dest, sendtag, comm, request);
+    MPI_Irecv(recvbuf, recvcount, recvtype, source, recvtag, comm, request);
+    
+    return 0;
+}
+
+
 static void exchange_ghost_cells(struct ngfs_2d *gfs)
 {
     int left_rank = gfs->domain.lower_x_rank;
@@ -115,19 +130,61 @@ static void exchange_ghost_cells(struct ngfs_2d *gfs)
     int bottom_rank = gfs->domain.lower_y_rank;
     int top_rank = gfs->domain.upper_y_rank;
 
-    if (right_rank) != INVALID_RANK { 
-        int *b = malloc(gfs->gs * gfs->ny * sizeof(double));
+    /*int *b = malloc(gfs->gs * gfs->ny * sizeof(double));
     
-        // NOTE: COULD BE POTENTIALLY TERRIBLE AND BAD: Maybe flattened along x instead of y
-        for (i=0; i < 2 * gfs->gs * gfs->ny; i++)
-        {
-    	    const int start = gf_indx_2d(gfs, gfs->nx - 2*gfs->gs - 1, 0);
-    	    b[i] = gfs->vars[0]->val[start + i];
-        }
-    	MPI_STATUS status;
-        MPI_Isendrecv(&b, gfs->gs * gfs->ny, MPI_DOUBLE, right_rank, 0, 
-    		  &gfs->vars[0]->val, gfs->gs * gfs->ny, MPI_DOUBLE, left_rank, 0,
-    		  MPI_COMM_WORLD, &status);
+    for (i=0; i < 2 * gfs->gs * gfs->ny; i++)
+    {
+    	const int start = gf_indx_2d(gfs, gfs->nx - 2*gfs->gs - 1, 0);
+    	b[i] = gfs->vars[0]->val[start + i];
+    }*/
+    MPI_Request request;
+    int sstart;
+    int rstart;
+    for (int j=0; j < gfs->ny; j++)
+    {
+	// Left to Right
+	if (right_rank != INVALID_RANK)
+	{
+	    sstart = gf_indx_2d(gfs, gfs->nx - 2*gfs->gs, j);
+	    rstart = gf_indx_2d(gfs, 0, j);
+            MPI_Isendrecv(&gfs->vars[0]->val[sstart], gfs->gs, MPI_DOUBLE, right_rank, 0, 
+    		          &gfs->vars[0]->val[rstart], gfs->gs, MPI_DOUBLE, left_rank, 0,
+    		          MPI_COMM_WORLD, &request);
+	}
+	
+	// Right to Left
+	if (left_rank != INVALID_RANK)
+	{
+	    sstart = gf_indx_2d(gfs, gfs->gs, j);
+	    rstart = gf_indx_2d(gfs, gfs->nx - gfs->gs, j);
+	    MPI_Isendrecv(&gfs->vars[0]->val[sstart], gfs->gs, MPI_DOUBLE, left_rank, 0,
+			  &gfs->vars[0]->val[rstart], gfs->gs, MPI_DOUBLE, right_rank, 0,
+			  MPI_COMM_WORLD, &request);
+	}
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    // Top to Bottom
+    if (bottom_rank != INVALID_RANK)
+    {
+	sstart = gf_indx_2d(gfs, 0, gfs->ny - 2*gfs->gs);
+	rstart = gf_indx_2d(gfs, 0, 0);
+
+	MPI_Isendrecv(&gfs->vars[0]->val[sstart], gfs->nx * gfs->gs, MPI_DOUBLE, bottom_rank, 0,
+		      &gfs->vars[0]->val[rstart], gfs->nx * gfs->gs, MPI_DOUBLE, top_rank, 0,
+		      MPI_COMM_WORLD, &request);
+    }
+
+    // Bottom to Top
+    if (top_rank != INVALID_RANK)
+    {
+	sstart = gf_indx_2d(gfs, 0, gfs->gs);
+	rstart = gf_indx_2d(gfs, 0, gfs->nx - gfs->gs);
+
+	MPI_Isendrecv(&gfs->vars[0]->val[sstart], gfs->nx * gfs->gs, MPI_DOUBLE, top_rank, 0,
+		      &gfs->vars[0]->val[rstart], gfs->nx * gfs->gs, MPI_DOUBLE, bottom_rank, 0,
+		      MPI_COMM_WORLD, &request);
     }
 }
 
